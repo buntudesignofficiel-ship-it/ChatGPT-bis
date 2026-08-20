287fd95a6ee9dfd2f3759a282f0179a139f73f3d
buntudesignofficiel-ship-it <buntudesignofficiel@gmail.com>
2026-08-21 00:46:22 +0200

    Bump build for scroll fix

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var BUILD = '20260820-020';
+  var BUILD = '20260820-021';
   var loader = document.getElementById('loader');
 
   function setStatus(msg){ if(loader) loader.textContent = msg; }
