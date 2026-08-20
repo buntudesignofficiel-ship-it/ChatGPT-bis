5c30e4e05a9038b5dda8acbc50ce9881621cb8c7
buntudesignofficiel-ship-it <buntudesignofficiel@gmail.com>
2026-08-20 23:35:38 +0200

    Bump build for refined intro

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var BUILD = '20260820-012';
+  var BUILD = '20260820-013';
   var loader = document.getElementById('loader');
 
   function setStatus(msg){ if(loader) loader.textContent = msg; }
