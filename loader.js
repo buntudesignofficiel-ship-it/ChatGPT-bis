2a10f13d0a645c816bcf7b4f09e88865a72d5939
buntudesignofficiel-ship-it <buntudesignofficiel@gmail.com>
2026-08-21 00:17:52 +0200

    Bump build for timeline fine tuning

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var BUILD = '20260820-018';
+  var BUILD = '20260820-019';
   var loader = document.getElementById('loader');
 
   function setStatus(msg){ if(loader) loader.textContent = msg; }
