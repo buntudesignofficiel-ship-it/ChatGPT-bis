7fc6cc2d17b6aa79f09e299dda9fe8539201c935
buntudesignofficiel-ship-it <buntudesignofficiel@gmail.com>
2026-08-20 23:53:38 +0200

    Bump build for timeline and carousel fixes

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var BUILD = '20260820-015';
+  var BUILD = '20260820-016';
   var loader = document.getElementById('loader');
 
   function setStatus(msg){ if(loader) loader.textContent = msg; }
