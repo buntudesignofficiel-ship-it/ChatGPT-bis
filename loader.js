142dfadb15d68e1e31062d7e1df3ef099b52b91c
buntudesignofficiel-ship-it <buntudesignofficiel@gmail.com>
2026-08-20 23:29:15 +0200

    Force refreshed intro typography build

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var BUILD = '20260820-011';
+  var BUILD = '20260820-012';
   var loader = document.getElementById('loader');
 
   function setStatus(msg){ if(loader) loader.textContent = msg; }
