f394664572449aadc8cdd2d4185532a69076fe81
buntudesignofficiel-ship-it <buntudesignofficiel@gmail.com>
2026-08-21 00:32:21 +0200

    Bump wedding site build 020

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var BUILD = '20260820-019';
+  var BUILD = '20260820-020';
   var loader = document.getElementById('loader');
 
   function setStatus(msg){ if(loader) loader.textContent = msg; }
