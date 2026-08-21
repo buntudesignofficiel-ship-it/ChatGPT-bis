48723c4021ad2c3f88b2f74978da87f1ed72ffaa
buntudesignofficiel-ship-it <buntudesignofficiel@gmail.com>
2026-08-21 22:53:35 +0200

    Bump build for safe RSVP counter RPC

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
-  var BUILD = '20260821-rsvp-counter1';
+  var BUILD = '20260821-rsvp-counter2';
   var loader = document.getElementById('loader');
 
   function setStatus(msg){ if(loader) loader.textContent = msg; }
